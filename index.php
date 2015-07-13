6a4058dcf19158a8c6f5a68bf3ea76fc973d2d98
emasters <emasters@cali.org>
2015-07-13 09:38:00 -0400

    Set from to POST; building navigation

--- index.php
+++ index.php
@@ -54,7 +54,12 @@
                 <li class="nav-header"> <a href="#" data-toggle="collapse" data-target="#userMenu">Lessons <i class="glyphicon glyphicon-chevron-down"></i></a>
                     <ul class="nav nav-stacked collapse in" id="userMenu">
                         <li class="active"> <a href="#"><i class="glyphicon glyphicon-th-list"></i> List</a></li>
-                        <li><a id="new" href="#"><i class="glyphicon glyphicon-plus"></i> New</a></li>
+                        <li><a id="new" href="#" data-toggle="collapse" data-target="#lessonMenu"><i class="glyphicon glyphicon-plus"></i> New</a></li>
+						<ul class="nav nav-stacked collapse in" id="lessonMenu">
+							<li>Info</li>
+							<li>Page</li>
+							<li>Question</li>
+						</ul>
                         <li><a href="#"><i class="glyphicon glyphicon-cog"></i> Options</a></li>
                         
                     </ul>
