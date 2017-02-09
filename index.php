9fe1d5562588eed60d0577eca79204a3dd80c72e
Elmer Masters <emasters@cali.org>
2017-02-09 17:51:50 -0500

    css for forms, some refactoring, getting closer

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+require ("./includes/config.php");
+$username = $_SESSION['username'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -14,16 +19,40 @@
 		<link href="css/styles.css" rel="stylesheet">
 	</head>
 	<body>
+		<!-- header -->
+<div id="top-nav" class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">CALI Author Web</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i> <?php echo $username; ?> <span class="caret"></span></a>
+                    <ul id="g-account-menu" class="dropdown-menu" role="menu">
+                        <li><a href="#">My Profile</a></li>
+                    </ul>
+                </li>
+                <li><a href="logout.php"><i class="glyphicon glyphicon-lock"></i> Logout</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- /container -->
+</div>
+<!-- /Header -->
 <?php
-session_start();
-require ("./includes/config.php");
 
 // check for logged in user
 if (isset($_SESSION['username'])){
 	$username = $_SESSION['username'];
 	include("./includes/home.php");
 } else {
-	include("./includes/register.php");
+	include("./includes/login.php");
 }
 
 
