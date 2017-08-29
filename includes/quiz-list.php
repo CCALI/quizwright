0ba512d58854e12c0019b79f82db32e23ffa30e9
Sam Goshorn <sam@cali.org>
2017-08-29 15:50:41 -0500

    Added more information to quiz/question lists

--- includes/quiz-list.php
+++ includes/quiz-list.php
@@ -6,10 +6,10 @@
 	<div class="panel panel-default">
 
 	<!-- Default panel contents -->
-	<div class="panel-heading">Quizzes</div>
+	<!--<div class="panel-heading">Quizzes</div>-->
 	<!-- Table -->
 	<table class="table">
-		<tr><th>ID#</th><th>Title</th><th>Description</th><th>Questions</th><th>More</th></tr>
+		<tr><th>Title</th><th>Description</th><th>&nbsp;</th><th>Published</th><th>&nbsp;</th><th>&nbsp;</th><th>ID#</th></tr>
 <?php
 //error_reporting(E_ALL); 
 require ("user-session.php");
@@ -27,34 +27,36 @@ if ($result = $mysqli->query($sql))
 		$publishdate=$row['publishdate'];
 		?>
 		<tr>
-			<td><?=$lid?></td>
 			<td> <?=$data['title']?></td>
 			<td> <?=oneLinerHTML($data['calidescription'])?></td>
 			
 			
 			<?php if ($published) { /* If published include only link to review and run */ ?>
-			
-			<td> <span class="badge"> <?=$numPages?> </span> </td>
-			
 			<td>
-				<a id="quiz-run"   class="btn btn-primary" target=_blank href="<?=$published?>">Run</a>
-				<a class="btn btn-default" href="./includes/quiz-detail.php?lid=<?=$lid?>">Details</a>
-				 
-				<span class="label label-default"  data-toggle="popover" title="Published"  data-placement="left"
-							data-trigger="hover" data-html="true" data-content="<?=$publishdate?>"><?=$publishdate?></span>
-				
-				
-				
-				
-				
-				</td>
+				<a class="btn btn-primary glyphicon glyphicon-play" title="Run my published quiz on www.cali.org" id="quiz-run"  target=_blank href="<?=$published?>"></a>
+			</td>
+			<td>
+				<span class="label label-default"  data-toggle="popover" title="Published"  data-placement="left" data-trigger="hover" data-html="true" data-content="Published to CALI website: <?=$publishdate?>"><?=$publishdate?></span>
+			</td>
+			<td nowrap>
+				<a class="btn btn-default" title="Review details of my quiz"  href="./includes/quiz-detail.php?lid=<?=$lid?>">Details</a>
+			</td>
+			
 			<?php } else { ?>
-			<td nowrap> <span class="badge"> <?=$numPages?> </span> <a class="btn btn-default"  href="./includes/quiz-page-order.php?lid=<?=$lid?>">+/-</a></td>
-			<td nowrap><a class="btn btn-primary"  href="./includes/quiz-detail.php?lid=<?=$lid?>">Details</a> 
-			<a class="btn btn-default"  href="./includes/quiz-info-edit.php?lid=<?=$lid?>">Desc</a> 
-			<a class="btn btn-default" target=_blank href="./cav/web/preview/index.php?quiz=<?=$lid?>">Preview</a> 
-			<a class="btn btn-default" target=_blank href="./book-data-xml.php?lid=<?=$lid?>">XML</a></td>
+			<td> </td>
+			<td>Unpublished</td>
+			<td nowrap>
+				<a class="btn btn-default" title="Review details of my quiz"  href="./includes/quiz-detail.php?lid=<?=$lid?>">Details</a> 
+				<a class="btn btn-default glyphicon glyphicon-pencil" title="Edit quiz information"   href="./includes/quiz-info-edit.php?lid=<?=$lid?>"> </a>
+				<a class="btn btn-default glyphicon glyphicon-th-list" title="Change questions in the quiz"   href="./includes/quiz-page-order.php?lid=<?=$lid?>"></a>
+				<a class="btn btn-default" title="Preview the quiz in the test viewer" target=_blank href="./cav/web/preview/index.php?quiz=<?=$lid?>">Preview</a> 
+				<a class="btn btn-default" title="View the XML data that makes up a quiz" target=_blank href="./book-data-xml.php?lid=<?=$lid?>">XML</a>
+			</td>
 			<?php } ?>
+			<td>
+				<span class="badge" data-toggle="popover" data-placement="left" data-trigger="hover" data-html="true" data-content="This quiz has <?=$numPages?> questions"> <?=$numPages?></span>
+			</td>
+			<td><?=$lid?></td>
 		</tr>
 		<?php 
 	}
