30262e1a942527039d7b9d47f91d958aba065037
Elmer <emasters@cali.org>
2020-02-24 16:31:31 -0500

    Can kill session after x amount of actvity. Needs adjusting and forced logout

--- includes/user-session.php
+++ includes/user-session.php
@@ -2,4 +2,21 @@
 	require ("./config.php");
 	session_start();
 	$uid = $_SESSION['uid'];
+	
+	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
+	}
+	$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+	
+	if (!isset($_SESSION['CREATED'])) {
+    $_SESSION['CREATED'] = time();
+	} else if (time() - $_SESSION['CREATED'] > 1800) {
+		// session started more than 30 minutes ago
+		session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
+		$_SESSION['CREATED'] = time();  // update creation time
+	}
+	
+	var_dump($_SESSION);
 ?>
